5783bb7d2fa3b95eecd54ce9a107a9eae9def347
Link4Electronics <link4electronics@gmail.com>
2026-03-30 16:18:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DESKTOP=/usr/share/applications/openbve.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/openbve /usr/lib/openbve /usr/share/games/openbve /usr/bin/mono
+quick-sharun /usr/bin/openbve /usr/lib/openbve /usr/share/games/openbve /usr/bin/mono /usr/lib/mono /usr/lib/libmono*.so*
 
 # Additional changes can be done in between here
 
